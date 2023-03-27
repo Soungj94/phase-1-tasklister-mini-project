06a471c330f9523a0158eebb7106b27e469f0004
Soungjae <soungj94@gmail.com>
2023-03-27 19:27:36 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    buildTodo(e.target["new-task-description"].value);
+    form.reset();
+  });
 });
+
+function buildTodo(todo) {
+  let p = document.createElement("p");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", deleteTodo);
+  btn.textContent = "x";
+  p.textContent = `${todo} `;
+  p.appendChild(btn); // 돔으로 보내기전에 p테그에 붙여서 보낼 수 있슴
+  console.log(p);
+  document.querySelector("#tasks").appendChild(p);
+}
+
+function deleteTodo(e) {
+  e.target.parentNode.remove();
+}
